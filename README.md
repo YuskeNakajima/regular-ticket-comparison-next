27e9269908c36e259a4903805983d3e3f4bcc623
yusuke_nakajima <www.yyworld@gmail.com>
2022-06-30 21:37:24 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,3 @@
+<div style="text-align: center;">
+    <h1>定期と切符の比較</h1>
+</div>
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>定期と切符の比較</title>
+                <meta name="description" content="定期と切符の比較"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+        </>
+    )
+}
+
+export default Home
